5a2e50bd3956f166ef9fdd93f19aab50dbce03ff
nalin <rmnsbandara@std.appsc.sab.ac.lk>
2024-10-28 17:39:53 +0530

    Student Management System

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <><div>
+
+    </div>
+     <h1>Student Management System</h1>
+     <p>Welcome to SMS</p>    </>
+   
+  )
+}
+
+export default App
